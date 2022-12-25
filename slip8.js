3ede05c2826423dcf24b4cb408d825160963bf8c
Girish kuwar <109171713+girishkuwar@users.noreply.github.com>
2022-12-25 15:43:56 +0530

    Update slip8.js

--- slip8.js
+++ slip8.js
@@ -1,15 +1,28 @@
 var mysql = require('mysql');
-var con = mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password : ""
-});
-con.connect (function (err) {
-    if (err) throw err;
-    console.log('connected');
-    con.query("CREATE DATABASE demotest123", function (err,result) {
-        if (err) throw err;
-        console.log ('Databse connected');
 
-    });
+var con = mysql.createConnection({
+  host : 'localhost', 
+  user : 'root', 
+  password : ''
 });
+
+con.connect(function (err){
+  if (err) throw err;
+  console.log('connected');
+  
+  con.query('CREATE DATABASE inventory', function(err, result){
+  if (err) throw err;
+  else {
+    console.log('Database Created');
+    con.query('use inventory');
+    var sql = 'CREATE TABLE customer (cid INT , cname VARCHAR(25) ,  phone INT(10) , city VARCHAR(25))';
+    con.query(sql, function (err, result){
+        if (err) throw err;
+        console.log('Table Created');
+    });
+  }
+
+  });
+});
+
+
