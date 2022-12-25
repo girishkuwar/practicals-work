3a489ddbe5e695534238f840023aa85087f2fb71
Girish kuwar <109171713+girishkuwar@users.noreply.github.com>
2022-12-25 14:26:41 +0530

    Update slip6.js

--- slip6.js
+++ slip6.js
@@ -1,17 +1,21 @@
 var http = require('http');
-var url = require('url');
 var fs = require('fs');
 
-http.createServer(function(req,res){
-    var q = url.parse(req.url,true);
-    var filename = "." + q.pathname;
-    fs.readFile(filename , function (err,data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type':'text/html'});
-            return res.end("404 Not Found");
+var server = http.createServer(function (req,res){
+    fs.open('ex.txt','r+', function(err, fd) {
+        if(err){
+            console.log(err);
+            return res.end('404 not found');
         }
-        res.writeHead(200, {'Content-Type':'text/html'});
-        res.write(data);
-        return res.end;
-    });
-}).listen(8081);
+        else{
+            console.log('file opened succesfully');
+            fs.readFile('ex2.txt', function(err,data){
+                if(!err){
+                    console.log('success');
+                    res.end(data);
+                }
+            });
+        }
+    })
+})
+server.listen(8080);
